debf6fd62f65318e7c28aec47c2f897ce51decbe
everywill <everywillatchina@gmail.com>
2018-06-28 13:54:29 +0800

    modify

--- bin/build.js
+++ bin/build.js
@@ -1,7 +1,8 @@
 const buildDLL = require('../lib/build/build-dll');
 const buildBusiness = require('../lib/build/build-business');
-const check = require('../lib/check');
+const checkProConf = require('../lib/build/check-project-config');
 const cs = require('../lib/console');
+const conf = require('../lib/config');
 
 cs.buildLog(`开始打包：${process.env.NODE_ENV === 'production' ? '发布' : '开发'}模式`, 'info');
 
@@ -12,8 +13,23 @@ const envMap = {
 
 const build = (type, options) => {
     type = type || 'dev'
+
     process.SUPERKAOLA_ENV = envMap[type]
-    process.SUPERMAN_ROOT = path.resolve(__dirname, '..');
+    process.SUPERMAN_ROOT = path.resolve(__dirname, '..')
+
+    let buildConf = helper.findConfig(
+        path.resolve(conf.root, `./${conf.CONF_FILE_NAME}`),
+        'build'
+    )
+
+    if (!checkProConf(buildConf)) helper.stop(true);
+
+    const { entry } = options;
+
+    if (entry) {
+        buildConf.entry = entry;
+    }
+
     const dllPromise = new Promise((resolve) => {
         if (process.env.npm_config_rebuild_dll) {
             buildDLL(() => {
@@ -22,11 +38,11 @@ const build = (type, options) => {
         } else {
             resolve(false);
         }
-    });
+    })
 
     dllPromise.then(() => {
         buildBusiness();
-    });
-};
+    })
+}
 
-build();
+build()
