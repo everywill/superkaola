ba267abd9b29dd6d963f03421c1cb1a42b07ff3e
everywill <everywillatchina@gmail.com>
2018-08-01 00:07:54 +0800

    fix index

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 #!/usr/bin/env node
+
 const program = require('commander')
 const pkg = require('./package.json')
 const init = require('./bin/init')
 const build = require('./bin/build')
 const upgrade = require('./bin/upgrade')
-const cs = require('./lib/console')
 
-const buildAction = type => {
+const buildAction = type => () => {
     build(type)
 }
 
 program
-.version(pkg.version)
+    .version(pkg.version, '-v, --version')
 
 // 初始化配置文件
-program
-.command('init [name]')
-.description('初始化配置')
-.action(init)
 
 program
-.command('dev')
-.description('开发环境打包')
-.action(buildAction('dev'))
+    .command('init')
+    .description('初始化配置')
+    .action(init)
 
 program
-.command('prd')
-.description('生产环境打包')
-.action(buildAction('prd'))
+    .command('dev')
+    .description('开发环境打包')
+    .action(buildAction('dev'))
+program
+    .command('prd')
+    .description('生产环境打包')
+    .action(buildAction('prd'))
 
 program
-.command('upgrade')
-.description('升级 superman 配置文件')
-.action(upgrade)
+    .command('upgrade')
+    .description('升级 superman 配置文件')
+    .action(upgrade)
 
 program.parse(process.argv)
